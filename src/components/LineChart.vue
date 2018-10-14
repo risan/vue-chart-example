624833494a732c8a70df1e31d9934d694475c404
risan <risanbagja@gmail.com>
2018-10-15 01:40:51 +0200

    LineChart component

---
+++ src/components/LineChart.vue
@@ -0,0 +1,46 @@
+<template>
+  <canvas ref="myChart" :width="width" :height="height"></canvas>
+</template>
+
+<script>
+import Chart from 'chart.js';
+
+export default {
+  name: 'monthly-sales-chart',
+  props: {
+    // The canvas's width.
+    width: {
+      type: Number,
+      validator: value => value > 0
+    },
+
+    // The canvas's height.
+    height: {
+      type: Number,
+      validator: value => value > 0
+    },
+
+    // The chart's data.labels
+    labels: Array,
+
+    // The chart's data.datasets
+    datasets: {
+      type: Array,
+      required: true
+    },
+
+    // The chart's options.
+    options: Object
+  },
+  mounted() {
+    new Chart(this.$refs.myChart, {
+      type: 'line',
+      data: {
+        labels: this.labels,
+        datasets: this.datasets
+      },
+      options: this.options
+    });
+  }
+}
+</script>
--- src/components/MonthlySalesChart.vue
+++ src/components/MonthlySalesChart.vue
@@ -1,25 +1,62 @@
 <template>
-  <canvas ref="myChart" width="500" height="300"></canvas>
+  <div>
+    <line-chart
+      :width="500"
+      :height="300"
+      :labels="['Jan', 'Feb', 'Mar', 'Apr', 'May']"
+      :datasets="$options.datasets"
+      :options="$options.options"
+    ></line-chart>
+  </div>
 </template>
 
 <script>
-import Chart from 'chart.js';
+import numeral from 'numeral';
+import LineChart from './LineChart';
+
+const datasets = [
+  {
+    label: '2018 Sales',
+    borderColor: 'rgba(50, 115, 220, 0.5)',
+    backgroundColor: 'rgba(50, 115, 220, 0.1)',
+    data: [300, 700, 450, 750, 450]
+  },
+  {
+    label: '2017 Sales',
+    borderColor: 'rgba(255, 56, 96, 0.5)',
+    backgroundColor: 'rgba(255, 56, 96, 0.1)',
+    data: [600, 550, 750, 250, 700]
+  }
+];
+
+const options = {
+  scales: {
+    yAxes: [{
+      ticks: {
+        beginAtZero: true,
+        callback: value => numeral(value).format('$0,0')
+      }
+    }]
+  },
+  tooltips: {
+    mode: 'index',
+    callbacks: {
+      label(tooltipItem, data) {
+        const label = data.datasets[tooltipItem.datasetIndex].label;
+        const value = numeral(tooltipItem.yLabel).format('$0,0');
+
+        return `${label}: ${value}`;
+      }
+    }
+  }
+};
 
 export default {
   name: 'monthly-sales-chart',
-  mounted() {
-    new Chart(this.$refs.myChart, {
-      type: 'line',
-      data: {
-        labels: ['Jan', 'Feb', 'Mar', 'Apr', 'May'],
-        datasets: [
-          {
-            label: '2018 Sales',
-            data: [300, 700, 450, 750, 450]
-          }
-        ]
-      }
-    });
+  datasets,
+  options,
+  components: {
+    LineChart
   }
 }
 </script>
