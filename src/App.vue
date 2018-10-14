638a491f9205401a313923726dae60e700c2ed96
risan <risanbagja@gmail.com>
2018-10-15 00:52:44 +0200

    Vue and Chart.js integration

---
+++ src/App.vue
@@ -0,0 +1,17 @@
+<template>
+  <div class="box">
+    <h2 class="title is-5">Monthly Sales Chart</h2>
+    <monthly-sales-chart></monthly-sales-chart>
+  </div>
+</template>
+
+<script>
+import MonthlySalesChart from './components/MonthlySalesChart';
+
+export default {
+  name: 'app',
+  components: {
+    MonthlySalesChart
+  }
+}
+</script>
---
+++ src/components/MonthlySalesChart.vue
@@ -0,0 +1,25 @@
+<template>
+  <canvas ref="myChart" width="500" height="300"></canvas>
+</template>
+
+<script>
+import Chart from 'chart.js';
+
+export default {
+  name: 'monthly-sales-chart',
+  mounted() {
+    new Chart(this.$refs.myChart, {
+      type: 'line',
+      data: {
+        labels: ['Jan', 'Feb', 'Mar', 'Apr', 'May'],
+        datasets: [
+          {
+            label: '2018 Sales',
+            data: [300, 700, 450, 750, 450]
+          }
+        ]
+      }
+    });
+  }
+}
+</script>
